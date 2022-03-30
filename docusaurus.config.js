f94355bbbcccaed7f50fa43ddad599b3ae474276
Barak Brudo <mr.lightning@gmail.com>
2022-03-30 10:30:54 +0300

    change config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,12 +8,12 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://MrBLightning.github.io',
+  url: 'https://scribe-security.github.io',
   baseUrl: '/docosaurus-scribe/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'MrBLightning', // Usually your GitHub org/user name.
+  organizationName: 'scribe-security', // Usually your GitHub org/user name.
   projectName: 'docosaurus-scribe', // Usually your repo name.
   trailingSlash: false,
   presets: [
